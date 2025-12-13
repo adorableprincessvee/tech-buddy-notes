ee49797cf92bf8b6500451fb38af074b0c2c21c7
Wealthometer <wealthhajoh87@gmail.com>
2025-12-13 01:42:35 +0100

    Update App.jsx
    Co-Authored-By: Vanessa <216306524+adorableprincessvee@users.noreply.github.com>
    Co-Authored-By: Vanessa2009-love <180121692+Vanessa2009-love@users.noreply.github.com>

--- tech-buddy-notes/src/App.jsx
+++ tech-buddy-notes/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Footer from './components/footer'
+import Header from './components/header'
 
 function App() {
 
   return (
     <>
-      <div className='bg-pink-700 rounded p-7 jus'>you</div>
+      
+      <Footer />
     </>
   )
 }
